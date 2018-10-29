dc653f2517955da28f540202afeb007d8f48f956
tensai-digital <dev@tensai.digital>
2018-10-29 11:38:02 -0600

    Initial commit

---
+++ src/components/common/CardSection.js
@@ -0,0 +1,28 @@
+// Import
+import React from 'react';
+import { View } from 'react-native';
+
+// Create component
+const CardSection = (props) => {
+    return (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
+    );
+};
+
+const styles = {
+    containerStyle: {
+        borderBottomWidth: 1,
+        padding: 5,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#ddd',
+        position: 'relative'
+    }
+};
+
+// Make available
+export { CardSection };
+
